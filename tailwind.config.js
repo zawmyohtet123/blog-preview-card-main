25fcdafae47b5a579e515335069240d41dce545a
zmyohtet159@gmail.com <zmyohtet159@gmail.com>
2023-12-29 19:56:24 +0630

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        "3xl": "8px 8px black",
+      },
+    },
+  },
+  plugins: [],
+};
